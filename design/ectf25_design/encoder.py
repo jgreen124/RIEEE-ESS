f410350a178776cdacb1644583ed80369a83d91c
OumarO1 <oouattara14@yahoo.com>
2025-03-20 20:17:10 -0400

    Update encoder.py

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -60,6 +60,9 @@ class Encoder:
         lib.hash(bytes,len(bytes),cipher)
         timestamper = rfc3161ng.RemoteTimestamper('http://freetsa.org/tsr', certificate=certificate_data)
         tsr = timestamper(data=data_file.read(), return_tsr=True)
+        msg = b'RIEEESA'
+        encryptor = PKCS1_OAEP.new(pubKey)
+        encrypted = encryptor.encrypt(msg)
         with open("data_file.tsr", "wb") as f:
         f.write(encoder.encode(tsr))
        
