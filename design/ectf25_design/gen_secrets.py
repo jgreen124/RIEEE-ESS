4f7f27177ee3293330c8c50589729f74b5ea1d74
OumarO1 <oouattara14@yahoo.com>
2025-03-29 09:35:54 -0400

    Update gen_secrets.py

--- design/ectf25_design/gen_secrets.py
+++ design/ectf25_design/gen_secrets.py
@@ -39,7 +39,7 @@ def gen_secrets(channels: list[int]) -> bytes:
     # You can change this to generate any secret material
     # The secrets file will never be shared with attackers
     secrets = {
-        password : bytes("aesion", "utf-8")
+        password : bytes("aesion", "utf-8"),
         salt = get_random_bytes(16)
         key = scrypt(password, salt, 16, N=2**14, r=8, p=10)
         keyPair = RSA.generate(3072)
