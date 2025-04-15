50f8413e536ad987557987dd6bfa14a9b729351c
OumarO1 <oouattara14@yahoo.com>
2025-04-15 12:14:42 -0400

    Update encoder.py

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -94,6 +94,6 @@ class Encoder:
 
        
 
-        payload = (struct.pack("<IQ", channel, timestamp) + struct.pack("<H", len(encrypted)) + encrypted + struct.pack("<I", len(json_bytes)) + json_bytes)
+        payload = (struct.pack("<IQ", channel, timestamp) + len(encrypted) + encrypted + len(ciphertext+tag+nonce) + ciphertext + tag + nonce)
         return payload
 
