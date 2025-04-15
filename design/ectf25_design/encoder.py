c43e561d7c2495ddbc1546cf1d7ac698e721078a
OumarO1 <oouattara14@yahoo.com>
2025-04-14 20:35:05 -0400

    Update encoder.py

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -90,6 +90,6 @@ class Encoder:
         result = json.dumps(dict(zip(json_k, json_v)))
        
 
-        return struct.pack("<IQ", channel, timestamp) + encrypted
+        return struct.pack("<IQ", channel, timestamp) + encrypted + + result.encode('utf-8')
 
 
