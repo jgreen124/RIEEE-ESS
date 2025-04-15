05b836d417800c10609e726b1d6150fe5a11c4ca
OumarO1 <oouattara14@yahoo.com>
2025-04-15 09:36:11 -0400

    Update encoder.py

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -94,8 +94,6 @@ class Encoder:
 
        
 
-        payload = (struct.pack("<IQ", channel, timestamp) + struct.pack("<H", len(encrypted)) + encrypted + struct.pack("<I", len(json_bytes)) + json_bytes)
-        stored = privKeyPEM 
+        payload = (struct.pack("<IQ", channel, timestamp) + encrypted + json_bytes)
         return payload
-        return privKeyPEM
 
